5a9186922c0d7528a53d6651630f53140088bb4d
danicurri <danicurri7603@gmail.com>
2018-04-22 18:27:13 +0200

    serilog,singleton and business facade additions

---
+++ Students.Business.Facade.WebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Students.Business.Facade.WebService.WebApiApplication" Language="C#" %>
--- StudentsXUnitTests/Program.cs
+++ StudentsXUnitTests/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Students.Business.Logic
+namespace StudentsXUnitTests
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
